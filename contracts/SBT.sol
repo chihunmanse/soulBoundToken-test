a01446a459eeab2da447d00e7b5848b1a4fd95df
chihunmanse <chihunmanse@gmail.com>
2022-11-05 22:35:22 +0900

    openzeppelin

---
+++ contracts/SBT.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (security/Pausable.sol)
+
+pragma solidity ^0.8.0;
+
+import "./openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./IERC5192.sol";
